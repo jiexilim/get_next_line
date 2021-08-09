3180f02085c0d2d94f25593eeb7c23b0aba3a50d
jiexilim <jiexilim.ljx@gmail.com>
2021-08-09 19:46:07 +0800

    Update .c files

--- get_next_line.c
+++ get_next_line.c
@@ -1,30 +1,20 @@
 #include "get_next_line.h"
 // #include <stdio.h>
+
 char	*trim(char **str)
 {
 	int		i;
 	char	*trimmed;
 	if (!*str || !(**str))
-	{
-		free(*str);
 		return (NULL);
-	}
 	i = 0;
 	while ((*str)[i] != '\n' && (*str)[i])
 		i++;
 	trimmed = malloc(i + 2);
 	if (!trimmed)
-	{
-		free(trimmed);
-		free(*str);
 		return (NULL);
-	}
 	ft_strlcpy(trimmed, (*str), i+2);
 	*str = ft_strdup(&(*str)[i+1]);
-	if (!trimmed)
-		free(trimmed);
-	if (!(*str))
-		free(*str);
 	return (trimmed);
 }
 
@@ -32,15 +22,13 @@ char	*get_next_line(int fd)
 {
 	static char	*store_str;
 	char		*buff;
+	char 		*temp;
 
 	if (fd < 0 || BUFFER_SIZE < 1)
 		return (NULL);
 	buff = malloc(BUFFER_SIZE + 1);
 	if (!buff)
-	{
-		free(buff);
 		return (NULL);
-	}
 	while (read(fd, buff, BUFFER_SIZE) > 0)
 	{
 		if (!store_str)
@@ -52,10 +40,12 @@ char	*get_next_line(int fd)
 				return (NULL);
 			}
 		}
-		store_str = ft_strjoin(store_str, buff);
+		temp = ft_strjoin(store_str, buff);
+		free(store_str);
+		store_str = temp;
 	}
 	free(buff);
-	return trim(&store_str);
+	return (trim(&store_str));
 }
 
 // int main()
