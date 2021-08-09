bebe14e72addfd39c3242dd14aabde462b21b961
jiexilim <jiexilim.ljx@gmail.com>
2021-08-09 16:40:09 +0800

    Update .c files

--- get_next_line.c
+++ get_next_line.c
@@ -1,14 +1,19 @@
 #include "get_next_line.h"
+#include <stdio.h>
 
 char	*trim(char **str)
 {
 	int		i;
 	char	*trimmed;
 	
+	if (!*str)
+		return (NULL);
 	i = 0;
 	while ((*str)[i] != '\n' && (*str)[i])
 		i++;
 	trimmed = malloc(i + 2);
+	if (!trimmed)
+		return (NULL);
 	ft_strlcpy(trimmed, (*str), i+2);
 	*str = ft_strdup(&(*str)[i+1]);
 	return (trimmed);
@@ -18,38 +23,33 @@ char	*get_next_line(int fd)
 {
 	static char	*store_str;
 	char		buff[BUFFER_SIZE];
-	int			read_buff;
 
 	if (fd < 0 || BUFFER_SIZE < 1)
 		return (NULL);
-	if (!store_str)
-		store_str = malloc(BUFFER_SIZE + 1);
-	if(!store_str)
-		return (NULL);
-	read_buff = read(fd, buff, BUFFER_SIZE);
-	if (read_buff < 0)
-		return (NULL);
-	while (read_buff > 0)
+	while (read(fd, buff, BUFFER_SIZE) > 0)
 	{
-		if (read_buff < 0)
-			return (NULL);
+		if (!store_str)
+		{
+			store_str = malloc(BUFFER_SIZE + 1);
+			if(!store_str)
+			{
+				free(store_str);
+				return (NULL);
+			}
+		}
 		store_str = ft_strjoin(store_str, buff);
-		read_buff = read(fd, buff, BUFFER_SIZE);
 	}
 	return trim(&store_str);
 }
 
-// #include <stdio.h>
-// int main()
-// {
-// 	int fd = open("file", O_RDWR);
-// 	printf("%d\n", fd);
-// 	int i = 0;
-// 	while (i < 3)
-// 	{
-// 		printf("%s", get_next_line(1000));
-// 		// get_next_line(fd);
-// 		i++;
-// 	}
-// 	return (0);
-// }
+int main()
+{
+	int fd = open("file", O_RDWR);
+	int i = 0;
+	while (i < 4)
+	{
+		printf("%s", get_next_line(fd));
+		i++;
+	}
+	return (0);
+}
